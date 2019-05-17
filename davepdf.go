1d436f7447fa61b81c67a6991d089090a82f89e7
Dave Barnes <phpdave11@gmail.com>
2019-05-17 15:27:13 -0500

    Bug fix for Text()

--- davepdf.go
+++ davepdf.go
@@ -3,24 +3,22 @@ package davepdf
 import (
 	"bytes"
 	"fmt"
-
 	//"github.com/phpdave11/gofpdi"
 )
 
 type Pdf struct {
-	n        int
-	w        *bytes.Buffer
-	catalog  *PdfCatalog
-	pageTree *PdfPageTree
-	xref     *PdfXrefTable
-	offsets  map[int]int
-	objects  []*PdfObject
-fontFamily string
-fontSize int
-x float64
-y float64
-page *PdfPage
-
+	n          int
+	w          *bytes.Buffer
+	catalog    *PdfCatalog
+	pageTree   *PdfPageTree
+	xref       *PdfXrefTable
+	offsets    map[int]int
+	objects    []*PdfObject
+	fontFamily string
+	fontSize   int
+	x          float64
+	y          float64
+	page       *PdfPage
 }
 
 type PdfCatalog struct {
@@ -88,12 +86,12 @@ func (pdf *Pdf) Text(text string) {
 	var instructions string
 
 	instructions += fmt.Sprintf("  %-30s %% begin text\n", "BT")
-	instructions += fmt.Sprintf("    %-28s %% set font family and font size\n", fmt.Sprintf("%s %d", pdf.fontFamily))
+	instructions += fmt.Sprintf("    %-28s %% set font family and font size\n", fmt.Sprintf("%s %d Tf", pdf.fontFamily, pdf.fontSize))
 	instructions += fmt.Sprintf("    %-28s %% set position to draw text\n", fmt.Sprintf("%f %f Td", pdf.x, pdf.y))
 	instructions += fmt.Sprintf("    %-28s %% write text\n", fmt.Sprintf("(%s)Tj", text))
 	instructions += fmt.Sprintf("  %-30s %% end text", "ET")
 
-    pdf.page.contents.data = append(pdf.page.contents.data, []byte(instructions)...)
+	pdf.page.contents.data = append(pdf.page.contents.data, []byte(instructions)...)
 }
 
 func (pdf *Pdf) AddPage() *PdfPage {
@@ -132,7 +130,7 @@ func (pdf *Pdf) Write() {
 	pdf.outln("  /Type /Catalog")
 	pdf.outln("  /Pages 2 0 R")
 	pdf.outln(">>")
-	pdf.outln("endobj")
+	pdf.outln("endobj\n")
 
 	// write page tree
 	pdf.newObj()
@@ -142,7 +140,7 @@ func (pdf *Pdf) Write() {
 	pdf.outln("  /Count 1")
 	pdf.outln("  /Kids [5 0 R]")
 	pdf.outln(">>")
-	pdf.outln("endobj")
+	pdf.outln("endobj\n")
 
 	// write resources
 	pdf.newObj()
@@ -156,7 +154,7 @@ func (pdf *Pdf) Write() {
 	//pdf.outln("    /GOFPDITPL0 7 0 R")
 	pdf.outln("  >>")
 	pdf.outln(">>")
-	pdf.outln("endobj")
+	pdf.outln("endobj\n")
 
 	// write fonts
 	pdf.newObj()
@@ -167,22 +165,28 @@ func (pdf *Pdf) Write() {
 	pdf.outln("  /Name /FONT1")
 	pdf.outln("  /BaseFont /Helvetica")
 	pdf.outln(">>")
-	pdf.outln("endobj")
-/*
-	// init gofpdi
-	fpdi := gofpdi.NewImporter()
-	fpdi.SetSourceFile("/Users/dave/Desktop/PDFPL110.pdf")
-	fpdi.SetNextObjectID(7)
-	tpl1 := fpdi.ImportPage(1, "/MediaBox")
+	pdf.outln("endobj\n")
+	/*
+		// init gofpdi
+		fpdi := gofpdi.NewImporter()
+		fpdi.SetSourceFile("/Users/dave/Desktop/PDFPL110.pdf")
+		fpdi.SetNextObjectID(7)
+		tpl1 := fpdi.ImportPage(1, "/MediaBox")
 
-	// get gofpdi template values
-	tplName, scaleX, scaleY, tX, tY := fpdi.UseTemplate(tpl1, 0, -400, 400, 0)
-	str := fmt.Sprintf("q 0 J 1 w 0 j 0 G 0 g q %.4F 0 0 %.4F %.4F %.4F cm %s Do Q Q", scaleX, scaleY, tX, tY, tplName)
-*/
-	str := ""
+		// get gofpdi template values
+		tplName, scaleX, scaleY, tX, tY := fpdi.UseTemplate(tpl1, 0, -400, 400, 0)
+		str := fmt.Sprintf("q 0 J 1 w 0 j 0 G 0 g q %.4F 0 0 %.4F %.4F %.4F cm %s Do Q Q", scaleX, scaleY, tX, tY, tplName)
+	*/
+
 	// add page
 	page := pdf.AddPage()
-	page.contents.data = []byte("BT /FONT1 18 Tf 0 0 Td (Hello World) Tj ET " + str)
+	//page.contents.data = []byte("BT /FONT1 18 Tf 0 0 Td (Hello World) Tj ET " + str)
+
+	pdf.SetFontFamily("Helvetica")
+	pdf.SetFontSize(18)
+	pdf.SetXY(10, 600)
+	pdf.SetXY(0, 0)
+	pdf.Text("Hello World!")
 
 	// write page
 	pdf.newObj()
@@ -194,7 +198,7 @@ func (pdf *Pdf) Write() {
 	pdf.outln("  /Contents 6 0 R")
 	pdf.outln("  /Resources 3 0 R")
 	pdf.outln(">>")
-	pdf.outln("endobj")
+	pdf.outln("endobj\n")
 
 	// write page contents
 	pdf.newObj()
@@ -205,26 +209,26 @@ func (pdf *Pdf) Write() {
 	pdf.outln("stream")
 	pdf.outln(string(page.contents.data))
 	pdf.outln("endstream")
-	pdf.outln("endobj")
+	pdf.outln("endobj\n")
 
-/*
-	// write imported objects
-	fpdi.PutFormXobjects()
+	/*
+		// write imported objects
+		fpdi.PutFormXobjects()
 
-	objs := fpdi.GetImportedObjects()
-	for i := 7; i < len(objs)+7; i++ {
-		pdf.newObj()
-		pdf.outln(fmt.Sprintf("%d 0 obj", i))
-		pdf.outln(objs[i])
-	}
-*/
+		objs := fpdi.GetImportedObjects()
+		for i := 7; i < len(objs)+7; i++ {
+			pdf.newObj()
+			pdf.outln(fmt.Sprintf("%d 0 obj", i))
+			pdf.outln(objs[i])
+		}
+	*/
 
 	// write xref
 	pdf.newObj()
 	pdf.outln("xref")
 	pdf.outln(fmt.Sprintf("0 %d", pdf.n))
 	pdf.outln("0000000000 65535 f ")
-	for i := 0; i < len(pdf.offsets) - 1; i++ {
+	for i := 0; i < len(pdf.offsets)-1; i++ {
 		pdf.outln(fmt.Sprintf("%010d 00000 n ", pdf.offsets[i]))
 	}
 
