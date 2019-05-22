c956122bb5f49c5bc51eb01bc60c0784bcd933bb
Dave Barnes <phpdave11@gmail.com>
2019-05-22 00:00:19 +0000

    Fix instructions

--- pdf.go
+++ pdf.go
@@ -70,11 +70,11 @@ func (pdf *Pdf) SetXY(x, y float64) {
 }
 
 func (pdf *Pdf) Text(text string) {
-	pdf.page.instructions.add(fmt.Sprintf("%-60s", "BT"), "begin text")
-	pdf.page.instructions.add(fmt.Sprintf("  %-58s", fmt.Sprintf("%s %d Tf", pdf.fontFamily, pdf.fontSize)), "set font family and font size")
-	pdf.page.instructions.add(fmt.Sprintf("  %-58s", fmt.Sprintf("%f %f Td", pdf.x, pdf.y)), "set position to draw text")
-	pdf.page.instructions.add(fmt.Sprintf("  %-58s", fmt.Sprintf("(%s)Tj", text)), "write text")
-	pdf.page.instructions.add(fmt.Sprintf("%-60s", "ET"), "end text")
+	pdf.page.instructions.add("BT", "begin text")
+	pdf.page.instructions.add(fmt.Sprintf("  %s %d Tf", pdf.fontFamily, pdf.fontSize), "set font family and font size")
+	pdf.page.instructions.add(fmt.Sprintf("  %f %f Td", pdf.x, pdf.y), "set position to draw text")
+	pdf.page.instructions.add(fmt.Sprintf("  (%s)Tj", text), "write text")
+	pdf.page.instructions.add("ET", "end text")
 }
 
 func (pdf *Pdf) Rect(x, y, w, h float64, mode string) {
