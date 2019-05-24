4cc9ff6df0755d6de5b5436b2d0e9b2bc0665c82
Dave Barnes <phpdave11@gmail.com>
2019-05-24 15:42:23 -0500

    gofmt

--- README.md
+++ README.md
@@ -10,34 +10,34 @@ package main
 import "github.com/phpdave11/davepdf"
 
 func main() {
-    pdf := davepdf.NewPdf()
+	pdf := davepdf.NewPdf()
 
 	// Page 1
-    pdf.AddPage()
+	pdf.AddPage()
 
-    tpl1 := pdf.ImportPage("/tmp/PDFPL110.pdf", 1, "/BleedBox")
-    pdf.UseImportedTemplate(tpl1, 362, 0, 250, 0)
+	tpl1 := pdf.ImportPage("/tmp/PDFPL110.pdf", 1, "/BleedBox")
+	pdf.UseImportedTemplate(tpl1, 362, 0, 250, 0)
 
 	// Page 2
-    pdf.AddPage()
+	pdf.AddPage()
 
-    pdf.SetFontFamily("Times-Roman")
-    pdf.SetFontSize(28)
-    pdf.SetXY(10, 600)
-    pdf.SetXY(0, 0)
-    pdf.Text("Hello World!")
+	pdf.SetFontFamily("Times-Roman")
+	pdf.SetFontSize(28)
+	pdf.SetXY(10, 600)
+	pdf.SetXY(0, 0)
+	pdf.Text("Hello World!")
 
-    pdf.SetFillColorCMYK(0, 81, 81, 45)
-    pdf.Rect(140, 140, 50, 50, "F")
+	pdf.SetFillColorCMYK(0, 81, 81, 45)
+	pdf.Rect(140, 140, 50, 50, "F")
 
-    pdf.SetFillColorCMYK(26, 0, 99, 13)
-    pdf.Circle(70, 140, 70, "F")
+	pdf.SetFillColorCMYK(26, 0, 99, 13)
+	pdf.Circle(70, 140, 70, "F")
 
 	// Curve (does not work yet)
 	pdf.Curve(5, 40, 30, 55, 70, 45, 60, 75, "")
 	pdf.Curve(80, 40, 70, 75, 150, 45, 100, 75, "")
 	pdf.Curve(140, 40, 150, 55, 180, 45, 200, 75, "")
 
-    pdf.Write()
+	pdf.Write()
 }
 ```
--- curve.go
+++ curve.go
@@ -23,18 +23,18 @@ func (pdf *Pdf) Curve(x0, y0, x1, y1, x2, y2, x3, y3 float64, style string) {
 	pdf.curve(x1, y1, x2, y2, x3, y3)
 }
 
-func (pdf *Pdf) point (x, y float64) {
-    // Sets a draw point
-    // Parameters:
-    // - x, y: Point
-	pdf.page.instructions.add(fmt.Sprintf("%.2F %.2F m", x * pdf.k, (pdf.h - y) * pdf.k), "set draw point")
+func (pdf *Pdf) point(x, y float64) {
+	// Sets a draw point
+	// Parameters:
+	// - x, y: Point
+	pdf.page.instructions.add(fmt.Sprintf("%.2F %.2F m", x*pdf.k, (pdf.h-y)*pdf.k), "set draw point")
 }
 
 func (pdf *Pdf) curve(x1, y1, x2, y2, x3, y3 float64) {
-    // Draws a Bézier curve from last draw point
-    // Parameters:
-    // - x1, y1: Control point 1
-    // - x2, y2: Control point 2
-    // - x3, y3: End point
-	pdf.page.instructions.add(fmt.Sprintf("%.2F %.2F %.2F %.2F %.2F %.2F c", x1 * pdf.k, (pdf.h - y1) * pdf.k, x2 * pdf.k, (pdf.h - y2) * pdf.k, x3 * pdf.k, (pdf.h - y3) * pdf.k), "draw a Bézier curve from last draw point")
+	// Draws a Bézier curve from last draw point
+	// Parameters:
+	// - x1, y1: Control point 1
+	// - x2, y2: Control point 2
+	// - x3, y3: End point
+	pdf.page.instructions.add(fmt.Sprintf("%.2F %.2F %.2F %.2F %.2F %.2F c", x1*pdf.k, (pdf.h-y1)*pdf.k, x2*pdf.k, (pdf.h-y2)*pdf.k, x3*pdf.k, (pdf.h-y3)*pdf.k), "draw a Bézier curve from last draw point")
 }
