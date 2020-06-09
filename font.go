e0df6529a85ca175387486b1b34c633bd2e34e07
Dave Barnes <phpdave11@gmail.com>
2020-06-09 14:26:28 -0500

    Validate font family - must be 1 of 14 standard fonts

--- font.go
+++ font.go
@@ -21,6 +21,34 @@ func (pdf *Pdf) newFont() *PdfFont {
 }
 
 func (pdf *Pdf) SetFontFamily(fontFamily string) {
+	validFonts := []string{
+		"Times-Roman",
+		"Times-Bold",
+		"Times-Italic",
+		"Times-BoldItalic",
+		"Courier",
+		"Courier-Bold",
+		"Courier-Oblique",
+		"Courier-BoldOblique",
+		"Helvetica",
+		"Helvetica-Bold",
+		"Helvetica-Oblique",
+		"Helvetica-BoldOblique",
+		"Symbol",
+		"ZapfDingbats"}
+
+	valid := false
+	for _, font := range validFonts {
+		if font == fontFamily {
+			valid = true
+			break
+		}
+	}
+
+	if !valid {
+		panic("Invalid font: " + fontFamily)
+	}
+
 	found := false
 
 	for _, font := range pdf.fonts {
